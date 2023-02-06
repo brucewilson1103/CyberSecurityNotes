d2cf6104e24e0bd9a1d03990a9bd543e82d7b41d
Bruce Wilson <brucewilson1103@gmail.com>
2023-02-05 20:04:27 -0500

    about

--- csnotes/src/pages/AboutPage.js
+++ csnotes/src/pages/AboutPage.js
@@ -1,5 +1,26 @@
-const AboutPage = () =>{
-    return <h5>This site is being created as a study guide to help anyone pass the Comptia Security +.</h5>
+const AboutPage = () => {
+    return <div>
+        <h5>This site is being created as a study guide to help anyone pass the Comptia Security +.</h5>
+
+        <p>From Comptia.org:</p>
+        <p>
+            CompTIA Security+ is the first security certification a candidate should earn.
+            It establishes the core knowledge required of any cybersecurity role and provides a springboard to intermediate-level
+            cybersecurity jobs. Security+ incorporates best practices in hands-on troubleshooting, ensuring candidates have practical
+            security problem-solving skills required to:
+        </p>
+        <ul>
+            <li> Assess the security posture of an enterprise environment and recommend and implement appropriate security solutions</li>
+            <li>Monitor and secure hybrid environments, including cloud, mobile, and IoT</li>
+            <li> Operate with an awareness of applicable laws and policies, including principles of governance, risk, and compliance</li>
+            <li>Identify, analyze, and respond to security events and incidents</li>
+        </ul>
+        <p>
+            Security+ is compliant with ISO 17024 standards and approved by the US DoD to meet directive 8140/8570.01-M requirements.
+            Regulators and government rely on ANSI accreditation, because it provides confidence and trust in the outputs of an
+            accredited program. Over 2.3 million CompTIA ISO/ANSI-accredited exams have been delivered since January 1, 2011.
+        </p>
+    </div>
 }
 
 export default AboutPage;
